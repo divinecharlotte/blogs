c64d2334635962f966075d0b1e77c270fa28d607
divine <divinemaina@gmail.com>
2024-02-21 18:24:56 +0200

    final touches on the app

--- app.js
+++ app.js
@@ -8,28 +8,34 @@ const dbURLI ='mongodb+srv://divinemaina:divine123@blogtut.j7qa3tk.mongodb.net/b
 mongoose.connect(dbURLI).then((result)=>app.listen(3000)).catch((err)=>console.log(err))
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
 
 // register view engine
 app.set('view engine', 'ejs');
 // app.set('views', 'myviews');
 
-app.get('/add-blog',(req,res)=>{
-const blog = new Blog({
-  title:'new blog1111',
-  snippet:'abou t m new blog',
-  body:'more about my blog'
+// app.get('/add-blog',(req,res)=>{
+// const blog = new Blog({
+//   title:'new blog1111',
+//   snippet:'abou t m new blog',
+//   body:'more about my blog'
+// });
+
+// blog.save().then((result)=>{res.send(result)
+
+// }).catch((err)=>{console.log(err);})
+// })
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
 });
 
-blog.save().then((result)=>{res.send(result)
-
-}).catch((err)=>{console.log(err);})
-})
-
+app.use('/blogs',blogRoutes)
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
-app.use(blogRoutes)
+
+
 // 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,42 +1,12 @@
-const express =require('express')
-const Blog =require('../models/blog')
+const express = require('express');
+const blogController = require('../controllers/blogContorller');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.redirect('/blogs');
-  });
-router.get('/blogs', (req, res) => {
-  
-    Blog.find().sort({createdAt:-1}).then((result)=>{
-      res.render('index', { title: 'all blogs', blogs:result });
-  
-    }).catch((err)=>{
-      console.log(err);
-    })
-  });
-  router.get('/blogs/:id', (req, res) => {
-    const id = req.params.id
-  
-    Blog.findById(id).then((result)=>{
-      res.render('detail', { title: 'blog details', blog:result });
+router.get('/create', blogController.blog_create_get);
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
 
-    }).catch((err)=>{
-      console.log(err);
-    })
-  });
-  router.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id
-  
-    Blog.findByIdAndDelete(id).then((result)=>{
-      res.json({redirect:'/blogs'})
-
-    }).catch((err)=>{
-      console.log(err);
-    })
-  });
-  
-  
-  router.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-  });
-  module.exports =router
+module.exports = router;
